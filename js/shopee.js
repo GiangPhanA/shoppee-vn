06c9bbd39d919ced539dbd6e31441de71afa688f
GiangPhanA <daperidot@gmail.com>
2022-01-06 11:25:02 +0700

    finish header

--- js/shopee.js
+++ js/shopee.js
@@ -6,21 +6,24 @@
 
 console.log('xin chào Giang');
 
-console.log('xin chào');
 //var register = document.getElementById("js-register");modal-login
 //const modal = document.getElementById('myModal');
 const modalRegisterForm = document.querySelector('.js-modal-register');
 const modalRegister = document.querySelector('.js-register');
+const callModalLogin = document.querySelector('.js-call-modal-login');
 closeRegister = document.querySelector('.js-modal-close');
 modalRegister.addEventListener('click',showModalRegister );
 closeRegister.addEventListener('click',hideModalRegister );
+callModalLogin.addEventListener('click',callShowModalLogin );
 
 
 const modalLoginForm = document.querySelector('.js-modal-login');
 const modalLogin = document.querySelector('.js-login');
+const callModalRegister = document.querySelector('.js-call-modal-register');
 closeLogin = document.querySelector('.js-modal-login-close');
 modalLogin.addEventListener('click',showModalLogin );
 closeLogin.addEventListener('click',hideModalLogin );
+callModalRegister.addEventListener('click',callShowModaRegister );
 
 // register
 function showModalRegister(){
@@ -42,4 +45,18 @@ function showModalLogin(){
 
 function hideModalLogin(){
     modalLoginForm.classList.remove('open');
+}
+
+// call Modal Register
+function callShowModalLogin(){
+    modalRegisterForm.classList.remove('open');
+    modalLoginForm.classList.add('open');
+
+}
+
+// call Modal Login
+function callShowModaRegister(){
+    // console.log('da click show register')
+    modalLoginForm.classList.remove('open');
+    modalRegisterForm.classList.add('open');
 }
