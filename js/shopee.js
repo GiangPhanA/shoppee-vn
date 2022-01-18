9fdec8ea7e69c774b19790283420051f2b24ce24
GiangPhanA <daperidot@gmail.com>
2022-01-18 15:07:39 +0700

    add modal input

--- js/shopee.js
+++ js/shopee.js
@@ -1,68 +1,76 @@
-const items = ["A","B","C"];
-const obj = {
-[items]: "Hello"
-}
-console.log(obj); 
-console.log(obj["A,B,C"]);
+
 
 // $(document).ready(function(){
 //     console.log('xin chào Giang');
 // })
 
 console.log('xin chào Giang');
+// modal input
+const modalInput = document.querySelector('.js-modal-input');
+closeModalInput = document.querySelector('.js-modal-header-close');
+closeModalInputOverlay = document.querySelector('.js-modal-overlay-input');
+closeModalInput.addEventListener('click',hidenModalInput );
+closeModalInputOverlay.addEventListener('click',hidenModalInput );
 
 //var register = document.getElementById("js-register");modal-login
 //const modal = document.getElementById('myModal');
-const modalRegisterForm = document.querySelector('.js-modal-register');
-const modalRegister = document.querySelector('.js-register');
-const callModalLogin = document.querySelector('.js-call-modal-login');
-closeRegister = document.querySelector('.js-modal-close');
-modalRegister.addEventListener('click',showModalRegister );
-closeRegister.addEventListener('click',hideModalRegister );
-callModalLogin.addEventListener('click',callShowModalLogin );
+
+// const modalRegisterForm = document.querySelector('.js-modal-register');
+// const modalRegister = document.querySelector('.js-register');
+// const callModalLogin = document.querySelector('.js-call-modal-login');
+// closeRegister = document.querySelector('.js-modal-close');
+// modalRegister.addEventListener('click',showModalRegister );
+// closeRegister.addEventListener('click',hideModalRegister );
+// callModalLogin.addEventListener('click',callShowModalLogin );
 
 
-const modalLoginForm = document.querySelector('.js-modal-login');
-const modalLogin = document.querySelector('.js-login');
-const callModalRegister = document.querySelector('.js-call-modal-register');
-closeLogin = document.querySelector('.js-modal-login-close');
-modalLogin.addEventListener('click',showModalLogin );
-closeLogin.addEventListener('click',hideModalLogin );
-callModalRegister.addEventListener('click',callShowModaRegister );
+// const modalLoginForm = document.querySelector('.js-modal-login');
+// const modalLogin = document.querySelector('.js-login');
+// const callModalRegister = document.querySelector('.js-call-modal-register');
+// closeLogin = document.querySelector('.js-modal-login-close');
+// modalLogin.addEventListener('click',showModalLogin );
+// closeLogin.addEventListener('click',hideModalLogin );
+// callModalRegister.addEventListener('click',callShowModaRegister );
+
+// close Modal Input
+function hidenModalInput(){
+    console.log('da click closeModalInput');
+    modalInput.classList.add('close');
+}
 
 // register
-function showModalRegister(){
-    console.log('da click');
-    modalRegisterForm.classList.add('open');
+// function showModalRegister(){
+//     console.log('da click');
+//     modalRegisterForm.classList.add('open');
     
-};
+// };
 
-function hideModalRegister(){
-    console.log('da click đóng modal');
-    modalRegisterForm.classList.remove('open');
-}
+// function hideModalRegister(){
+//     console.log('da click đóng modal');
+//     modalRegisterForm.classList.remove('open');
+// }
 
 // login
-function showModalLogin(){
-    console.log('da click modal login');
-    modalLoginForm.classList.add('open');
-}
+// function showModalLogin(){
+//     console.log('da click modal login');
+//     modalLoginForm.classList.add('open');
+// }
 
-function hideModalLogin(){
-    modalLoginForm.classList.remove('open');
-}
+// function hideModalLogin(){
+//     modalLoginForm.classList.remove('open');
+// }
 
 // call Modal Register
-function callShowModalLogin(){
-    modalRegisterForm.classList.remove('open');
-    modalLoginForm.classList.add('open');
+// function callShowModalLogin(){
+//     modalRegisterForm.classList.remove('open');
+//     modalLoginForm.classList.add('open');
 
-}
+// }
 
 // call Modal Login
-function callShowModaRegister(){
-    // console.log('da click show register')
-    modalLoginForm.classList.remove('open');
-    modalRegisterForm.classList.add('open');
-}
+// function callShowModaRegister(){
+//     // console.log('da click show register')
+//     modalLoginForm.classList.remove('open');
+//     modalRegisterForm.classList.add('open');
+// }
 
